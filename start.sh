31d560a65246ac164178f29afe5245f70969db66
JiC54lab <105932652+JiC54lab@users.noreply.github.com>
2022-06-22 11:40:17 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/JiC54lab/Black-Child-Pro.git /Black-Child-Pro
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /Black-Child-Pro
 fi
-cd /EvaMaria
+cd /Black-Child-Pro
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
